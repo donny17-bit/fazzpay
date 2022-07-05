21ea4bd42e796e4a7471ba6923a3427b17ae3163
Donny <doniwahyu14@gmail.com>
2022-07-06 04:12:12 +0700

    change env

--- next.config.js
+++ next.config.js
@@ -5,15 +5,10 @@ const nextConfig = {
     URL_BACKEND: "https://fazzpay.herokuapp.com",
     URL_BACKEND2: "https://jsonplaceholder.typicode.com",
     URL_IMAGE: "https://res.cloudinary.com/dd1uwz8eu/image/upload/v1653276449/",
-    URL_DEFAULT_IMG:
-      "https://cdn-icons.flaticon.com/png/512/2102/premium/2102633.png?token=exp=1657032483~hmac=3bcf0fdc2d5be666f0720b618a089935",
+    URL_DEFAULT_IMG: "https://cdn-icons-png.flaticon.com/512/747/747545.png",
   },
   async rewrites() {
     return [
-      {
-        source: "/",
-        destination: "/home",
-      },
       {
         source: "/login",
         destination: "/auth/login",
